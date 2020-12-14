12ccf3af91ff3f2716af2154894ccf4fe5db18b8
joelmcdaniel <joelmcdbme@gmail.com>
2020-12-14 00:15:41 -0800

    serve TLS at example.com

--- main.go
+++ main.go
@@ -2,72 +2,95 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/subosito/gotenv"
 )
 
 var host string
-var port string
-var key string
-var mSResp map[string]snippetResp
+var mS map[string]snippet
 
-type snippetReq struct {
-	Name      string
-	ExpiresIn int
-	Snippet   string
-}
-
-type snippetResp struct {
-	URL       string `json:"url"`
-	Name      string `json:"name"`
-	Now       string `json:"now"`
-	ExpiresAt string `json:"expires_at"`
-	Snippet   string `json:"snippet"`
+type snippet struct {
+	URL     string `json:"url"`
+	Name    string `json:"name"`
+	Expires string `json:"expires_at"`
+	Snippet string `json:"snippet"`
 }
 
 func init() {
-	gotenv.Load()
-	host = os.Getenv("HOST")
-	port = os.Getenv("PORT")
 
-	key = "recipe"
-	mSResp = make(map[string]snippetResp)
+	host = "example.com"
+	mS = make(map[string]snippet)
 }
 
 func main() {
 
-	r := mux.NewRouter()
-	r.HandleFunc("/snippets", snippetsHandler)
-	r.HandleFunc(fmt.Sprintf("/snippets/{%s}", key), snippetsHandler)
+	crt := flag.String("cert", "", "the path the the cert file")
+	key := flag.String("key", "", "the path to the key file")
 
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	flag.Parse()
+
+	r := mux.NewRouter()
+	s := r.Host(host).PathPrefix("/snippets").Subrouter()
+	s.HandleFunc("", postHandler).Methods("POST")
+	s.HandleFunc("/{recipe}", getHandler).Methods("GET")
+
+	err := http.ListenAndServeTLS(":443", *crt, *key, r)
+	if err != nil {
+		log.Fatal("ListenAndServeTLS: ", err)
+	}
 }
 
-// SnippetsHandler ...
-func snippetsHandler(w http.ResponseWriter, r *http.Request) {
+func postHandler(w http.ResponseWriter, r *http.Request) {
 
-	vars := mux.Vars(r)
+	var sReq snippet
+	err := json.NewDecoder(r.Body).Decode(&sReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-	if key, ok := vars[key]; ok {
-		// /snippets/{recipe} route hit
+	t1 := time.Now()
+	t2 := t1.Add(time.Second * 30)
 
-		if s, ok := mSResp[key]; ok {
-			// recipe snippet already exists
+	sResp := snippet{
+		URL:     fmt.Sprintf("https://%s/snippets/%s", host, sReq.Name),
+		Name:    sReq.Name,
+		Expires: t2.Format(time.RFC3339),
+		Snippet: sReq.Snippet,
+	}
 
+	mS[sReq.Name] = sResp
+
+	resp, err := json.Marshal(sResp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(resp))
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+
+	for k := range mux.Vars(r) {
+		if s, ok := mS[k]; ok {
+			// recipe snippet already exists in map
 			t1 := time.Now()
-			t2, err := time.Parse(time.RFC3339, s.ExpiresAt)
+			t2, err := time.Parse(time.RFC3339, s.Expires)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
 			if t1.After(t2) {
 				// recipe snippet expired
+				delete(mS, s.Name)
 				w.WriteHeader(http.StatusNotFound)
 			} else {
 				resp, err := json.Marshal(s)
@@ -80,41 +103,8 @@ func snippetsHandler(w http.ResponseWriter, r *http.Request) {
 				w.Write([]byte(resp))
 			}
 		} else {
-			// recipe snippet doesn't exist
+			// recipe snippet doesn't exist in map
 			w.WriteHeader(http.StatusNotFound)
 		}
-
-	} else {
-		// /snippets route hit
-
-		var sReq snippetReq
-		err := json.NewDecoder(r.Body).Decode(&sReq)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		t1 := time.Now()
-		t2 := t1.Add(time.Second * 30)
-
-		sResp := snippetResp{
-			URL:       fmt.Sprintf("https://%s:%s/snippets/%s", host, port, sReq.Name),
-			Name:      sReq.Name,
-			ExpiresAt: t2.Format(time.RFC3339),
-			Snippet:   sReq.Snippet,
-		}
-
-		mSResp[sReq.Name] = sResp
-
-		resp, err := json.Marshal(sResp)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(resp))
 	}
-
 }
